cb38384880f3442fb1e4faf359c1e982f51ea001
Ivy <ivyzhao335@gmail.com>
2025-11-06 23:22:00 -0500

    update

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,4 @@
+console.log('loaded')
 const PLAYLIST_ID = '37i9dQZEVXbLRQDuF5jeBp'
 
 
@@ -9,7 +10,7 @@ async function getToken() {
         const response = await fetch('/api/spotify/token')
         const data = await response.json()
         token = data.access_token
-        localStorage.setItem('spotifyToken'.token)
+        localStorage.setItem('spotifyToken', token)
     }
     return token
 }
@@ -17,7 +18,7 @@ async function getToken() {
 async function getTopPlaylist() {
     const token = await getToken()
     try {
-        const response = await fetch(`https://api.spotify.com/v1/playlists/${PLAYLIST_ID}`, {
+        const response = await fetch(`/api/spotify/playlist/${PLAYLIST_ID}`, {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
@@ -35,3 +36,5 @@ async function getTopPlaylist() {
         console.error(error)
     }
 }
+
+//after loading in tracks data, we need to get the top 10 artist ids, their popularity, and images of artist. it'll show the ranking of the top 10, and when you click into an artist, there will be an 'about me' section, and a sample of their most popular song will play(?)
