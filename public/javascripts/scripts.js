b68e6953d436781971648f60ec0b31e33c29e680
DanielM0217 <daniel.martinez27@login.cuny.edu>
2025-11-10 17:43:40 -0500

    working basic map and added two buttons which are to be used to specify status of tablets

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,2 +1,10 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// call to leaflet for the tile
+//set view is starting location
+var map = L.map('map').setView([40.8, -73.9], 11);
 document.getElementById('map')
+
+//adds the viewable part of the map aka tile
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
