ee32e56b9097a7dc2bb109cf054c7704d9eacf19
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2025-11-10 22:42:47 -0500

    added addmarker function

--- public/stylesheets/main.js
+++ public/stylesheets/main.js
@@ -2,6 +2,7 @@
 async function getData(){
     const response = await fetch('/trees');
     const data = await response.json();
+    topTenFavTrees(data.data);
     console.log(data);
 }
 getData();
@@ -10,6 +11,7 @@ getData();
 async function getForestTreeData(){
     const response = await fetch('https://data.cityofnewyork.us/resource/hn5i-inap.json');
     const data = await response.json();
+    // addMarker(data);
     console.log(data);
 }
 getForestTreeData();
@@ -25,6 +27,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
+//adds markers to the map
+function addMarker(info){
+    //iterates through the entire forestry api and goes through the desired length of trees and adds a marker to the map
+    for(let x = 0; x < info.length; x++){
+        L.marker([info[x].location.coordinates[1], info[x].location.coordinates[0]]).addTo(map)
+        .bindPopup(info[x].genusspecies);
+    }
+}
+
+//join the TreeID & ObjectID
+function joinIDs(){
+
+
+}
+
 //function that displays top 10 least favorite trees
 function tenLeastFav(){
 
@@ -32,8 +49,13 @@ function tenLeastFav(){
 
 
 //function that displays the top 10 most favorite trees
-function topTenFavTrees(){
+function topTenFavTrees(Treeinfo){
 
+    for(let i = 0; i < 5171; i++){
+        if(Treeinfo[i].numberOfTimesFavorited >=  2){
+            console.log(Treeinfo[i].numberOfTimesFavorited);
+        }
+    }
 }
 
 
